f8602e29caaf053e21ea33bfbb292adf6d13e80a
MahnazJ <md.jazayeri.sev@gmail.com>
2022-02-19 16:37:48 +0100

    changes

--- views/contact/form.view.php
+++ views/contact/form.view.php
@@ -9,27 +9,36 @@
     <form method="POST" name="frmContact" class="register-form" onsubmit="return false;">
             <?= generateFormTokenHTML() ?>
 
-            <div class="form-group mb-2">
+            <div class="form-group">
                 <label for="first_name" class="form-label register-form text-style">First Name</label>
                 <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" required />
             </div>
 
-            <div class="form-group mt-1">
+            <div class="form-group">
                 <label for="last_name" class="form-label register-form text-style">Last Name</label>
                 <input type="test" class="form-control" name="last_name" id="last_name" placeholder="Last Name" required />
             </div>
 
-            <div class="form-group mb-1">
+            <div class="form-group">
                 <label for="email" class="form-label register-form text-style">Email</label>
                 <input type="email" class="form-control" name="email" id="email" placeholder="Email" required />
             </div>
 
-            <div class="form-group mb-1">
-                <label for="subject" class="form-label register-form text-style">Subject</label>
-                <input type="subject" class="form-control" name="subject" id="subject" placeholder="Subject" required />
+            <div class="form-group">
+                <label for="subject" class="form-label register-form text-style">
+                <select class="form-select" name="subject" id="subject" placeholder="Subject" aria-label="Default select example">
+                    <option selected>Choose a subject</option>
+                    <option value="1">Order</option>
+                    <option value="2">Delivery and Returns</option>
+                    <option value="3">Product information</option>
+                    <option value="4">Sustainability</option>
+                    <option value="5">Warranty</option>
+                </select>
+                </label>
+
             </div>
              
-            <div class="form-group mb-1">
+            <div class="form-group">
                   <label for="content" class="form-label register-form text-style">Message</label> 
                      <span id="userMessage-info" class="info"></span><br/>
                    <textarea name="content" id="content" class="input-field" cols="60" rows="6"></textarea>
--- views/site/aboutus.view.php
+++ views/site/aboutus.view.php
@@ -1,9 +1,18 @@
 <?php require 'views/partials/header.view.php' ?>
 <div class="container">
-<h1 class="text-center mt-3 mb-2 buttonstyle">About Us</h1>
+    <div class="row">
+    <div clas="col">
+<h1 class="text-center mt-3 mb-2 buttonstyle long-text">About Us</h1>
     <p class="text-center mt-3 mb-2 buttonstyle">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit voluptas earum impedit dignissimos quod magnam debitis quisquam, molestias nam facilis eaque quia tempore qui voluptate blanditiis provident expedita? Impedit, dolore!</p>
+    </div>
+    </div>
 
-
+    <div class="col">
+    <aside class="text-center mt-3 mb-2 buttonstyle long-text">
+        <blockquote><h4>Why is play so important?</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint blanditiis, vitae ad illum dolorum tempore pariatur molestiae, corrupti numquam alias accusantium odit debitis aliquid fugiat incidunt consequuntur minus expedita aut?</p></blockquote>
+    </aside>
+    </div>
 </div>
 <?php require 'views/partials/footer.view.php' ?>
 
